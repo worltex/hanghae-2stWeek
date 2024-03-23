b7f72e3016ddafe80f1952f9ca2c206ed4f42aaa
doggai10 <doggai77@gmail.com>
2024-03-23 17:30:58 +0900

    project setting

---
+++ src/test/java/com/hangahae/st/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hangahae.st.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
