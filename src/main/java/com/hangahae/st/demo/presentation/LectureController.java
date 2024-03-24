9fc1b40b8a6ea2ae403370e2286e22d39dcb26e8
doggai10 <doggai77@gmail.com>
2024-03-24 17:24:20 +0900

    add 로직 방향 주석 수정

--- src/main/java/com/hangahae/st/demo/presentation/LectureController.java
+++ src/main/java/com/hangahae/st/demo/presentation/LectureController.java
@@ -13,19 +13,18 @@ public class LectureController {
 
     @PostMapping
     public void applyLecture(@RequestBody ApplyLectureRequest request) {
-        //1. 강의 등록한 인원 가능한 인원 조회
-        //1.2 해당 강의에 등록 등록 대기중 및 등록 완료한 인원 count
-        //1.3 30명 넘은 경우 에러 처리
+        //1. Lecture 조회 (total_enrollment, current_enrollment) LOCK 30초 정도
+        //1.2 current_enrollment>total_enrollment 넘는 경우 에러 처리
 
 
         //2. 동일한 신청자의 중복 요청이 있는지 확인
         //2.1 동일한 요청이 있는 경우 에러 처리
-        //2.2 없는 경우 Enrollment 테이블에 status REGISTERING
+        //2.2 없는 경우 Lecture Registration 테이블에 status REGISTERING
 
 
-        //3. 자기 자신의 요청 + REGISTERED 된 인원이 최대인원보다 큰지 확인
-        //3.1 큰 경우 FAILED 처리 혹은 row 삭제 처리 => 에러 처리
-        //3.2 작은 경우 COMPLETED 처리
+        //3. current_enrollemnt+1 > total_enrollment인지 확인
+        //3.1 큰 경우 Lecture Registration row 삭제 처리 => 에러 처리
+        //3.2 작은 경우 COMPLETED 처리 및 current_enrollment +1 처리
     }
 
     @GetMapping
