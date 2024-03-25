de34f62f8809062a6396eab705ccadaa55a3a0e1
doggai10 <doggai77@gmail.com>
2024-03-25 14:18:46 +0900

    add dto

---
+++ src/main/java/com/hangahae/st/demo/dto/LectureDto.java
@@ -0,0 +1,12 @@
+package com.hangahae.st.demo.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class LectureDto {
+
+    private int maxEnrollment;
+    private int currentEnrollment;
+}
